f181e79de333d7a23fdadddb9e662f78584341d6
WrRaThY <woohaas@gmail.com>
2020-09-19 21:00:53 +1200

    project init

---
+++ src/test/java/priv/rdo/graphql/GraphqlPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.rdo.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
